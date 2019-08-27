b03caa173fb6c99d18447ba6ef32e35b29a1624a
TwinProduction <twin@twinnation.org>
2019-08-27 16:24:41 -0400

    Reduce memory footprint by passing pointers instead of new objects

--- choice.go
+++ choice.go
@@ -26,16 +26,16 @@ func pick(question string, choicesToPickFrom []string, config *Config) (string, 
 	if len(choicesToPickFrom) == 0 {
 		return "", errors.New("no choices to choose from")
 	}
-	var choices []Choice
+	var choices []*Choice
 	for i, choice := range choicesToPickFrom {
-		choices = append(choices, Choice{Id: i, Value: choice, Selected: i == 0})
+		choices = append(choices, &Choice{Id: i, Value: choice, Selected: i == 0})
 	}
 	if err := termbox.Init(); err != nil {
 		return "", err
 	}
 	termbox.SetInputMode(termbox.InputEsc)
 	defer termbox.Close()
-	var selectedChoice = &choices[0]
+	var selectedChoice = choices[0]
 	for {
 		render(question, choices, config, selectedChoice)
 		switch ev := termbox.PollEvent(); ev.Ch {
@@ -64,30 +64,30 @@ func pick(question string, choicesToPickFrom []string, config *Config) (string, 
 	}
 }
 
-func move(choices []Choice, increment int) *Choice {
+func move(choices []*Choice, increment int) *Choice {
 	for i, choice := range choices {
 		if choice.Selected {
 			if i+increment < len(choices) && i+increment >= 0 {
 				choices[i].Selected = false
 				choices[i+increment].Selected = true
-				return &choices[i+increment]
+				return choices[i+increment]
 			}
 			// Choice didn't change, return it
-			return &choice
+			return choice
 		}
 	}
 	panic("Something went wrong")
 }
 
-func moveUp(choices []Choice) *Choice {
+func moveUp(choices []*Choice) *Choice {
 	return move(choices, -1)
 }
 
-func moveDown(choices []Choice) *Choice {
+func moveDown(choices []*Choice) *Choice {
 	return move(choices, 1)
 }
 
-func render(question string, options []Choice, config *Config, selectedChoice *Choice) {
+func render(question string, options []*Choice, config *Config, selectedChoice *Choice) {
 	check(termbox.Clear(config.BackgroundColor.toTermboxAttribute(), config.BackgroundColor.toTermboxAttribute()))
 	_, maximumThatCanBeDisplayed := termbox.Size()
 	lineNumber := 0
